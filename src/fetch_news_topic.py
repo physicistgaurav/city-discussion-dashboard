8b4f7b42e255c1d19888861df26d74d3e68cde53
physicistgaurav <gauravrijal03@gmail.com>
2024-08-21 22:54:27 +0545

    fix: saved response to json

--- src/fetch_news_topic.py
+++ src/fetch_news_topic.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 def fetch_top_news_topic(city_name, api_key):
 
@@ -20,6 +21,10 @@ def fetch_top_news_topic(city_name, api_key):
     else:
         raise Exception("Failed to fetch news topics")
 
+def save_topics_to_file(topics, filename):
+    with open(filename, 'w') as f:
+        json.dump(topics, f, indent=4)
+
 if __name__ == "__main__":
     api_key = os.getenv('NEWS_API_KEY')
 
@@ -29,6 +34,7 @@ if __name__ == "__main__":
     city = input("Enter the name of the city: ")
     try:
         top_news = fetch_top_news_topic(city, api_key)
+        save_topics_to_file(top_news, 'data/topics.json')
         print(f"Top 5 news topics in {city}:")
         for index, headline in enumerate(top_news, start=1):
             print(f"{index}. {headline}")
